0009e52882c695e8c7f2399484dc008c42f92daf
Lucian Tarba <lucian.tarba@ctbav.ro>
2019-05-31 15:18:56 +0300

    function added
    added two more functions
    
    updated function 2's name

---
+++ authors/function.java
@@ -0,0 +1,10 @@
+public void Function(){
+	System.out.println("This is an empty function");
+}
+
+public void Function222(){
+	System.out.println("This is another empty function");
+}
+public void Function3(){
+	System.out.println("This is another empty function");
+}
